1b7ae8abfbc6b133389ac4966662da55d8ddee54
David Logie <d@djl.im>
2011-03-28 20:33:08 +0100

    Cleaner namespacing.

--- src/chrome/content/gotourl.js
+++ src/chrome/content/gotourl.js
@@ -1,30 +1,32 @@
 window.addEventListener("load", function(event) {
     var menu = document.getElementById("contentAreaContextMenu");
     if (menu) {
-        menu.addEventListener("popupshowing", gotoURLInit, false);
+        menu.addEventListener("popupshowing", gotoURL.init, false);
     }
 }, false);
 
-function gotoURLInit(event) {
-    var menuItems = ['current', 'window', 'tab'];
-    for (var i=0; i < menuItems.length; i++) {
-        var item = document.getElementById('gotourl-' +  menuItems[i]);
-        item.hidden = !gContextMenu.isTextSelected;
-    }
-}
+var gotoURL = {
+    init: function() {
+        var menuItems = ['current', 'window', 'tab'];
+        for (var i=0; i < menuItems.length; i++) {
+            var item = document.getElementById('gotourl-' +  menuItems[i]);
+            item.hidden = !gContextMenu.isTextSelected;
+        }
+    },
 
-function gotoURL(where) {
-    var focusedWindow = document.commandDispatcher.focusedWindow;
-    var url = focusedWindow.getSelection();
+    go: function(where) {
+        var focusedWindow = document.commandDispatcher.focusedWindow;
+        var url = focusedWindow.getSelection();
 
-    switch(where) {
-    case "current":
-        gBrowser.loadURI(url);
-        break;
-    case "window":
-        window.open(url);
-        break;
-    default:
-        window.getBrowser().addTab(url);
+        switch(where) {
+        case "current":
+            gBrowser.loadURI(url);
+            break;
+        case "window":
+            window.open(url);
+            break;
+        default:
+            window.getBrowser().addTab(url);
+        }
     }
 }
